fd0b5bb4858fb3b5dbdd5b33cd6224cdfbe97623
Valdionor Junior <valdionorjunior@outlook.com>
2019-07-20 21:10:38 -0300

    Add troca de imagens ao jogardo escolher

--- app/src/main/java/com/juniorrodrigues/jokenpo/MainActivity.java
+++ app/src/main/java/com/juniorrodrigues/jokenpo/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +16,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void selectRock(View view){}
+    public void selectRock(View view){
+        this.optionSelected("Pedra");
+    }
 
-    public void selectPaper(View view){}
+    public void selectPaper(View view){
+        this.optionSelected("Papel");
+    }
 
-    public void selectScissors(View view){}
+    public void selectScissors(View view){
+        this.optionSelected("Tesoura");
+    }
 
-    public void optionSelected(){
+    public void optionSelected(String optionUser){
+        String [] options = {"Pedra", "Papel", "Tesoura"};
 
+        ImageView imageResult = (ImageView) findViewById(R.id.imageResult);
+
+        switch( options[new Random().nextInt(options.length)]) {
+            case "Pedra": imageResult.setImageResource(R.drawable.pedra); break;
+            case "Pepel": imageResult.setImageResource(R.drawable.papel); break;
+            case "Tesoura": imageResult.setImageResource(R.drawable.tesoura); break;
+//            default: imageResult.setImageResource(R.drawable.padrao); break;
+        }
     }
 }
